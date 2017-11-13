0505a6cb3996132065f2c6236624f1b4d4d11c8d
Jesse Waite <jesse_waite@hotmail.com>
2017-11-12 16:17:13 -0800

    updates

--- permute.py
+++ permute.py
@@ -1,7 +1,29 @@
-alphabet = raw_input("Enter symbol alphabet over which to permute: ")
-length = raw_input("Enter length of string: ")
+from __future__ import print_function
 
 
+try:
+	get_input = raw_input
+except:
+	get_input = input
+
+#Recursive permutation of an alphabet for some length
+def permute(sigma, length, prefix):
+	#sigma = list(set([c for c in alphabet]))
+	
+	if length <= 0:
+		print(prefix)
+		return
+	
+	for c in sigma:
+		pre = prefix + c
+		#print(pre, end="")
+		permute(sigma, length-1, pre)
+		
+	
+alphabet = get_input("Enter symbol alphabet over which to permute: ")
+length = int(get_input("Enter length of string: "))
+
+permute(alphabet, length,"")
 
 
 
