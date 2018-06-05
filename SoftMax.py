5744566b5bc797d791ff7e2d1904a9f48ee0a437
niceyeti <jesse_waite@hotmail.com>
2018-06-05 12:04:27 -0700

    updates

--- SoftMax.py
+++ SoftMax.py
@@ -10,13 +10,17 @@ Keep this modular, s.t. it can be used for recurrent neural net implementation.
 
 import sys
 from sklearn import linear_model, metrics
+from sklearn.datasets import fetch_mldata
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import tarfile
+
+dtype = np.float64
 
 
 def _zeros(shape):
-	return np.zeros(shape, dtype="float")
+	return np.zeros(shape, dtype=dtype)
 
 """
 Loads the ocr data, which is a sequential structured prediction dataset.
@@ -62,7 +66,7 @@ def loadOcrData(dataPath="./mldata/ocr_fold0_sm_test.txt", linear=False):
 				for i in range(len(xStr)):
 					if xStr[i] not in ["0","1"]:
 						print("WARNING unmapped x vector value (must be in 0/1): "+xStr[i])
-					xVec[i,0] = float(xStr[i])
+					xVec[i,0] = dtype(xStr[i])
 				oneHot = oneHotMap[classLabel]
 				#print("Example:")
 				#print(str(xVec.shape))
@@ -80,6 +84,55 @@ def loadOcrData(dataPath="./mldata/ocr_fold0_sm_test.txt", linear=False):
 	return dataset, oneHotMap
 
 """
+Mnist data consists of pixels in 28x28 images with pixels 0-255, so each
+x input is 28x28=784 length vector in Reals. The mnist csv data consists of the first
+column a label in 0-9, and the rest of the values are the 784 length real vector.
+
+The dataset is returned as a list of examples, where each example is a list of numpy vector pairs (x,y), where
+|x| is some input dimension, and |y| is a k-length one-hot encoded vector.
+The character label map is also returned, for decoding class labels from one-hot vector encodings. This dict is of the form
+'a' -> one-hot-vector. 
+"""
+def loadMnistData(dataset="train"):
+	dataPairs = [] #list of (x,y) pairs, where x is a 1x784 real value vector, and y is a 1x10 one-hot encoded vector.
+	classDict = dict([(str(i), i) for i in range(0,10)])
+	numClasses = len(classDict.keys())
+	xDim = 784
+
+	if dataset == "train":
+		tarPath = "./mldata/mnist_train.csv.tar.gz"
+		dataPath = "./mldata/mnist_train.csv"
+	else:
+		tarPath = "./mldata/mnist_test.csv.tar.gz"
+		dataPath = "./mldata/mnist_test.csv"
+
+	#extract the datasets
+	tar = tarfile.open(tarPath, "r:gz")
+	tar.extractall("./mldata")
+	tar.close()
+
+	print("Reading mnist data...")
+	with open(dataPath,"r") as ifile:
+		for line in ifile:
+			if len(line) > 20:
+				vals = line.split(",")
+				#build the one-hot target class vector
+				y = np.zeros(shape=(numClasses,1))
+				yIndex = classDict[vals[0]]
+				y[yIndex,0] = 1.0
+				#build the 784 length x vector
+				xs = [int(x) for x in vals[1:]]
+				x = _zeros((xDim,1))
+				for xi, i in enumerate(xs):
+					x[i,0] = dtype(xi)
+				x = np.array(xs, dtype=dtype)
+				dataPairs.append((x,y))
+
+	print("Got {} training pairs, classDict {}".format(len(dataPairs), str(classDict)))
+
+	return dataPairs, classDict
+
+"""
 Applies the softmax function to the input vector z. Vector z is not modified.
 This applies the "safe" version of softmax where the input is shifted by constant
 c, where c = max(z), for numerical stability.
@@ -89,26 +142,6 @@ Returns: A vector y_hat, produced by softmax.
 def _softmax(z):
 	e_z = np.exp(z - np.max(z))
 	return e_z / np.sum(e_z)
-
-"""
-Calculates the Jacobian of partial derivatives per the softmax cross-entropy loss between the output
-y_hat and the true vector y.
-
-See Eli Bendersky's write-up on softmax regression for a very good derivation. But in short,
-the softmax function Jacobian consists of elements i, j:
-	S_i (1 - S_i)  if i == j
-	-S_j * S_i     if i != j
-
-Here S_i and S_j are the ith and jth respective outputs of the softmax vector y_hat (so, scalars).
-	
-The jacobian wrt the 
-	
-Returns: the Jacobian, incidentally of the same dim as weight matrix W
-"""	
-#def _jacobian_W(x, y, y_hat):
-	
-
-
 	
 """
 Trains according to straightforward multiclass softmax regression of one-hot encoded
@@ -121,47 +154,63 @@ not the structured-prediction training data format where each example is itself 
 The numpy dimensions of these pairs is x.shape=(m,1) and y.shape=(n,1)
 """
 def SoftMaxTraining(trainingData):
-	xdim = trainingData[0][0].shape[0]
+	xdim = trainingData[0][0].shape[0] + 1 #plus one for the bias
 	ydim = trainingData[0][1].shape[0]
 	print("Xdim: %d   Ydim: %d" % (xdim,ydim))
 	W = _zeros((ydim, xdim))
-	#b = _zeros((ydim, 1))
-	#z is holds the vector Wx+b, before softmax is applied
-	#z = numpy.zeros((ydim, 1))
-	
+	dW_prev = _zeros((ydim, xdim))
+
 	iterations = 0
 	maxEpochs= 50
 	print("Training...")
 	n = len(trainingData)
-	eta = 0.1
+	eta = dtype(0.1)
+	etaDecay = dtype(0.2)
+	useWeightDecay = False
+	weightDecay = dtype(0.999999)
+	momentum = 0.2
+	useMomentum = True
 	avgLosses = []
 	
 	while iterations < maxEpochs:
-		if iterations > 2:
-			eta = 0.01
-		if iterations > 8:
-			eta = 0.001
-		if iterations > 15:
-			eta = 0.0001
-		if iterations > 20:
-			eta = 0.00001
-	
+		if iterations % 5 == 4:
+			eta *= etaDecay
+		
 		ncorrect = 0
 		losses = []
 		for i in range(n):
 			example = trainingData[random.randint(0,n-1)]
 			x = example[0]
+			#append the bias constant
+			x = np.append(x,1.0)
 			y = example[1]
 			z = W.dot(x)
 			#print("Zdim: "+str(z.shape))
 			y_hat = _softmax(z)
+			#a numpy nuisance
+			y_hat = y_hat.reshape(y.shape)
 			#J = _jacobian(x, y, y_hat) #get the Jacobian of partial derivatives wrt
 			#print(str(y_hat.shape)+"<y_hat    y:"+str(y.shape))
+			#print("{} \n {}".format(y_hat, y))
 			#exit()
-			loss = -np.log(y_hat[np.argmax(y),0])
+			p_y_hat = y_hat[np.argmax(y),0]
+			if p_y_hat > 0.0:
+				loss = -np.log(y_hat[np.argmax(y),0])
+			else:
+				loss = 10.0
 			losses.append( loss )
-			dW = eta * np.outer(y - y_hat , x)
-			W += dW
+
+			#get the weight update
+			dW = np.outer(y - y_hat , x)
+			if useMomentum:
+				W += eta * dW + momentum * dW_prev
+				dW_prev = dW
+			else:
+				W += dW
+
+			if useWeightDecay:
+				W = weightDecay * W
+
 			#Training tracking
 			if np.argmax(y) == np.argmax(y_hat): #track correct predictions
 				ncorrect += 1
@@ -195,7 +244,13 @@ def SklearnMultinomialRegression(training, classDict):
 
 
 def main():
-	training, classDict = loadOcrData("./mldata/ocr_fold0_sm_test.txt",linear=True)
+	if "--ocr" in sys.argv:
+		training, classDict = loadOcrData("./mldata/ocr_fold0_sm_test.txt",linear=True)
+	elif "--mnist" in sys.argv:
+		training, classDict = loadMnistData("train")
+	else:
+		print("ERROR must pass --ocr or --mnist to select dataset")
+		exit()
 	print(str(training[0]))
 	SoftMaxTraining(training)
 	#SklearnMultinomialRegression(training, classDict)
---
+++ mldata/readme.txt
@@ -0,0 +1,3 @@
+The mnist data csv columns are classLabel, pixel0, pixel1, pixel2... pixel783. So the first column is a character
+0-9, and the rest is the input vector of pixels in 0-255.
+
